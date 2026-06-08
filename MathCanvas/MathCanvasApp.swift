4acbc764748f46fa5cdaf689292d808d55b99829
ZhiyiMan <zhiyiman61@gmail.com>
2026-06-08 08:16:08 +0800

    Initial Commit

---
+++ MathCanvas/MathCanvasApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathCanvasApp.swift
+//  MathCanvas
+//
+//  Created by Jane on 2026/6/8.
+//
+
+import SwiftUI
+
+@main
+struct MathCanvasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
